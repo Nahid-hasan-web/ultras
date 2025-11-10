3cee3b341fc3afde35d0bae26766bcf300386488
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-10 18:45:07 +0600

    responsive done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,85 +1,136 @@
-import Link from 'next/link'
-import React from 'react'
-import logo from '../../public/images/logo.png'
-import Image from 'next/image'
+'use client'
+import Link from "next/link";
+import React, { useState } from "react";
+import logo from "../../public/images/logo.png";
+import Image from "next/image";
 import { IoMdSearch } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { FaRegHeart } from "react-icons/fa";
-
 import { AiOutlineShopping } from "react-icons/ai";
-
-
+import { FaBars } from "react-icons/fa6";
 
 const Navbar = () => {
-
-    const NavItems = [
-        {
-            navContent:"Women",
-            navLink:'/',
-        },
-        {
-            navContent:"Men",
-            navLink:'/',
-        },
-        {
-            navContent:"Kids",
-            navLink:'/',
-        },
-        {
-            navContent:"Accessories",
-            navLink:'/',
-        },
-        {
-            navContent:"Collections",
-            navLink:'/',
-        },
-        {
-            navContent:"Brand",
-            navLink:'/',
-        },
-        {
-            navContent:"Sale",
-            navLink:'/',
-        },
-        {
-            navContent:"Blog",
-            navLink:'/',
-        },
-    ]
-
-
+    const [showNav , setShowNav] = useState(false)
+  const NavItems = [
+    {
+      navContent: "Women",
+      navLink: "/",
+    },
+    {
+      navContent: "Men",
+      navLink: "/",
+    },
+    {
+      navContent: "Kids",
+      navLink: "/",
+    },
+    {
+      navContent: "Accessories",
+      navLink: "/",
+    },
+    {
+      navContent: "Collections",
+      navLink: "/",
+    },
+    {
+      navContent: "Brand",
+      navLink: "/",
+    },
+    {
+      navContent: "Sale",
+      navLink: "/",
+    },
+    {
+      navContent: "Blog",
+      navLink: "/",
+    },
+  ];
 
   return (
-    <nav className='pt-[19px] pb-[30px] border-t border-[#EDEDED]'>
+    <>
+      <nav className="pt-[19px] pb-[30px] border-t border-[#EDEDED] hidden lg:block">
         <div className="container">
-            <div className="nav_row flex justify-between items-center flex-wrap">
-                <Link href={'/'} className=' w-[80px] lg:w-[124px] '><Image src={logo} alt='logo'/></Link>
-                <div className='flex gap-[47px] items-center'>
-                    {
-                        NavItems.map((item , i)=>(
-
-                            <Link 
-                            className='text-[17px] font-medium font-inter   text-[#191919]'
-                            key={i}
-                            href={ item.navLink}
-                            
-                            >{item.navContent}</Link>
-                        ))
-                    }
-                </div>
-                <div className='flex items-center gap-4 '>
-                    <button className='text-xl text-[#191919]'><IoMdSearch/></button>
-                    <Link href={'#'} className='text-xl text-[#191919]'><FaRegUser/></Link>
-                    <Link href={'#'} className='text-xl text-[#191919]'><FaRegHeart/></Link>
-                    <button className='text-2xl text-[#191919] relative'><AiOutlineShopping/>
-                        <span className='w-[14px] h-[14px] flex justify-center items-center text-[10px] font-normal font-inter text-[#191919] bg-[#D1E0EA] absolute bottom-[-6px] rounded-full right-0'>1</span>
-                    </button>
-                </div>
+          <div className="nav_row flex justify-between items-center flex-wrap">
+            <Link href={"/"} className=" w-[80px] lg:w-[124px] ">
+              <Image src={logo} alt="logo" />
+            </Link>
+            <div className="flex gap-[47px] items-center">
+              {NavItems.map((item, i) => (
+                <Link
+                  className="text-[17px] font-medium font-inter   text-[#191919]"
+                  key={i}
+                  href={item.navLink}
+                >
+                  {item.navContent}
+                </Link>
+              ))}
             </div>
+            <div className="flex items-center gap-4 ">
+              <button className="text-xl text-[#191919]">
+                <IoMdSearch />
+              </button>
+              <Link href={"#"} className="text-xl text-[#191919]">
+                <FaRegUser />
+              </Link>
+              <Link href={"#"} className="text-xl text-[#191919]">
+                <FaRegHeart />
+              </Link>
+              <button className="text-2xl text-[#191919] relative">
+                <AiOutlineShopping />
+                <span className="w-[14px] h-[14px] flex justify-center items-center text-[10px] font-normal font-inter text-[#191919] bg-[#D1E0EA] absolute bottom-[-6px] rounded-full right-0">
+                  1
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
+      </nav>
+      {/* ---------- phone navbar */}
+      <nav className="pt-[19px] pb-[30px] border-t border-[#EDEDED] lg:hidden ">
+        <div className="container">
+          {/* ---- logo */}
+          <div className="nav_row flex justify-between items-center flex-wrap">
+            <Link href={"/"} className=" w-[80px] lg:w-[124px] ">
+              <Image src={logo} alt="logo" />
+            </Link>
+            {/* ------- icons  */}
+            <div className="flex items-center gap-4 ">
+              <button className="text-xl text-[#191919]">
+                <IoMdSearch />
+              </button>
+              <Link href={"#"} className="text-xl text-[#191919]">
+                <FaRegUser />
+              </Link>
+              <Link href={"#"} className="text-xl text-[#191919]">
+                <FaRegHeart />
+              </Link>
+              <button className="text-2xl text-[#191919] relative">
+                <AiOutlineShopping />
+                <span className="w-[14px] h-[14px] flex justify-center items-center text-[10px] font-normal font-inter text-[#191919] bg-[#D1E0EA] absolute bottom-[-6px] rounded-full right-0">
+                  1
+                </span>
+              </button>
+            </div>
+            {/* ------- menu button */}
+            <button onClick={()=>setShowNav(!showNav)} className="text-xl   relative">
+              <FaBars />
+            </button>
+              <div  className={`flex gap-[15px] flex-col items-center absolute w-[330px] rounded-[10px] p-4 bg-[#D1E0EA] ${showNav? 'right-[15px]':' right-[450px]'} duration-[.4s] top-[150px]`}>
+                {NavItems.map((item, i) => (
+                  <Link
+                    className="text-[17px] font-medium font-inter   text-[#191919]"
+                    key={i}
+                    href={item.navLink}
+                  >
+                    {item.navContent}
+                  </Link>
+                ))}
+              </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
 
-    </nav>
-  )
-}
-
-export default Navbar
+export default Navbar;
