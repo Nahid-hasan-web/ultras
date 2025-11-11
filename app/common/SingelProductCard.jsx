23b7ab7b78b8daf9d0eb11afd3cf935041e4b2f0
Nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-11 10:10:38 +0600

    banner slider added

---
+++ app/common/SingelProductCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingelProductCard = () => {
+  return (
+    <></>
+  )
+}
+
+export default SingelProductCard
--- app/components/Banner.jsx
+++ app/components/Banner.jsx
@@ -4,18 +4,26 @@ import bannerImage from '../../public/images/banner_image.png'
 import Image from 'next/image'
 import Slider from 'react-slick'
 import "slick-carousel/slick/slick.css";
+import { MdDisabledVisible, MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+
 const Banner = () => {
       const settings = {
             dots: true,
             infinite: true,
             speed: 500,
             slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToScroll: 1,
+            dots:false,
+            nextArrow:    <div><button className='w-[54px] h-[54px] bg-[#fff] flex justify-center items-center text-2xl absolute top-[50%] lg:right-[-28px] translate-[-50%] z-[30]'><MdOutlineKeyboardArrowRight/></button></div>,
+            prevArrow:    <div><button className='w-[54px] h-[54px] bg-[#fff] flex justify-center items-center text-2xl absolute top-[50%] lg:left-[28px] translate-[-50%] z-[30]'><MdOutlineKeyboardArrowLeft/></button></div>
         };
   return (
-    <>
-        <section id='Banner'>
-            <div className="slider-container">
+    <>  
+        <section id='Banner' className=' overflow-hidden'>
+
+
+            <div className="slider-container  relative">
              <Slider {...settings}>
                     <div className='w-full h-screen'><Image src={bannerImage} alt='banner image'/></div>
                     <div className='w-full h-screen'><Image src={bannerImage} alt='banner image'/></div>
