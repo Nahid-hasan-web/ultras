0d994e5caa5fda4abec5ad370a2fddcb0924291d
nahid-hasan-web <nahidhasanniyan@gmail.com>
2025-11-20 18:37:55 +0600

    add to cart api intregration

---
+++ app/common/AddToCartButton.jsx
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+
+const AddToCartButton = ({ productId }) => {
+  const handelClick =async() => {
+    try {
+     const apiRes = await fetch("http://localhost:8000/cart/addCart", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body:JSON.stringify({
+            userId:"68c836cf11cd2114930d7c52",
+            cartItem:[{productId}]
+        })
+      });
+
+      const data = await apiRes.text()
+    
+      console.log(data)
+      alert('product added to cart')
+
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <button
+        onClick={handelClick}
+        className="text-[17px] font-medium font-inter active:scale-[1.1] text-[#191919] flex items-center gap-2.5  "
+      >
+        add To Cart <BsArrowRight />
+      </button>
+    </>
+  );
+};
+
+export default AddToCartButton;
--- app/common/SingelProductCard.jsx
+++ app/common/SingelProductCard.jsx
@@ -5,32 +5,12 @@ import { BsArrowRight } from "react-icons/bs";
 import { BsArrowsFullscreen } from "react-icons/bs";
 import { FaRegHeart } from "react-icons/fa";
 import { IoMdArrowDropdown } from "react-icons/io";
+import AddToCartButton from './AddToCartButton';
 
 const SingelProductCard = ({porductImage , productName , productPrice , productStock , productId }) => {
 
 
-const handelClick = async () => {
-  try {
-    const response = await fetch('http://localhost:8000/cart/addCart', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        userId: "68c836cf11cd2114930d7c52",
-        cartItem: [{ productId }]
-      })
-    });
 
-    const data = await response.text(); // <-- use text() instead of json()
-    console.log("Cart updated:", data);
-    alert("Product added to cart successfully!");
-  } catch (error) {
-    console.error("Error adding to cart:", error);
-    alert("Failed to add product to cart");
-  }
-};
   return (
     <>
       <div className='w-full lg:w-[400px] rounded-1.5  bg-white'>
@@ -38,8 +18,8 @@ const handelClick = async () => {
        <div className='p-2.5 h-[512px] overflow-hidden relative  group border border-[#EAEAEA]'>
         <Image fill src={porductImage} alt={productName}/>
           {/* add to cart button */}
-          <div className='w-full lg:w-[348px] h-[65px] bg-red-500 absolute px-[27px] duration-[.4s] bottom-[-100px] group-hover:bottom-9 left-[50%] translate-x-[-50%] justify-between flex items-center'>
-            <button onClick={handelClick} className='text-[17px] font-medium font-inter active:scale-[1.1] text-[#191919] flex items-center gap-2.5  '>add T0 Cart <BsArrowRight/></button>
+          <div className='w-full lg:w-[348px] h-[65px] bg-white absolute px-[27px] duration-[.4s] bottom-[-100px] group-hover:bottom-9 left-[50%] translate-x-[-50%] justify-between flex items-center'>
+            <AddToCartButton productId={productId}/>
             <div className='flex  items-center gap-[29px]'>
                <button className=' relative' ><BsArrowsFullscreen/>
                 <p className=' w-[88px] px-1.5 py-2 bg-[#292929] text-[14px] font-medium text-white  rounded-[3px] absolute -left-10 top-[-50px]'>Quick view <IoMdArrowDropdown className=' absolute left-10 -bottom-3 text-xl text-black'/></p>
